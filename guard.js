ebbd408e6964512e8f45ee2592588177123f9086
grizou-Sport <grizou@gmx.ch>
2026-06-30 19:51:39 +0200

    fix: configure clean auth guard

--- guard.js
+++ guard.js
@@ -1,26 +1,16 @@
-console.log("GUARD: fichier chargé");
-
 async function protectPage() {
-  console.log("GUARD: protectPage démarre");
-
   if (!window.momentumDB) {
     console.error("GUARD: momentumDB introuvable");
-    alert("Erreur: Supabase n'est pas chargé");
-    return;
-  }
-
-  const { data, error } = await momentumDB.auth.getSession();
-
-  console.log("GUARD: session", data.session);
-  console.log("GUARD: error", error);
-
-  if (error || !data.session) {
-    console.log("GUARD: redirection login");
     window.location.href = "login.html";
     return;
   }
 
-  console.log("GUARD: utilisateur connecté");
+  const { data, error } = await window.momentumDB.auth.getSession();
+
+  if (error || !data.session) {
+    window.location.href = "login.html";
+    return;
+  }
 }
 
 protectPage();
--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
-const SUPABASE_URL = "sb_publishable_seh3MhAWh4Ov0DWYtAeH1w_TKHMCb5f";
-const SUPABASE_ANON_KEY = "sb_secret_AsdaENC-c8a1YhjdaM8Qaw_xDj5iOkfY";
+const SUPABASE_URL = "https://njcqcpyiiibudlalnzoa.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_seh3MhAWh4Ov0DWYtAeH1w_TKHMCb5f";
 
-window.momentumDB = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-console.log("Supabase chargé :", window.momentumDB);
+  window.momentumDB = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+  );
